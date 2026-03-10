758b10747fdfa961800d551d7ac0e297413247b5
cicalecristian <cicalecristian@gmail.com>
2026-03-10 14:48:06 +0100

    second and third ex.

--- src/es2/Main.java
+++ src/es2/Main.java
@@ -1,7 +1,23 @@
 package es2;
 
+import java.util.Scanner;
+
 public class Main {
+
+    static String stringa(int number) {
+        return switch (number) {
+            case 0 -> "zero";
+            case 1 -> "uno";
+            case 2 -> "due";
+            case 3 -> "tre";
+            default -> "numero non valido";
+        };
+    }
+
     static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("inserisci un numero da 0 a 3: ");
+        int number = Integer.parseInt(scanner.nextLine());
+        System.out.println(stringa(number));
     }
 }
--- src/es3/Main.java
+++ src/es3/Main.java
@@ -1,7 +1,29 @@
 package es3;
 
-public class Main {
-    static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+    static String[] stringa(String parola) {
+        return parola.split("");
+    }
+
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci una stringa (:q per terminare)");
+        String parola = scanner.nextLine();
+
+        while (!parola.equals(":q")) {
+
+            String[] array = stringa(parola);
+            String risultato = String.join(",", array);
+
+            System.out.println(risultato);
+
+            parola = scanner.nextLine();
+        }
+
+        System.out.println("programma terminato");
     }
 }
