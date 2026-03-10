acbb05e9b848238b16b894b64cd81b88ef88dace
cicalecristian <cicalecristian@gmail.com>
2026-03-10 15:20:08 +0100

    fourth ex.

--- src/es4/Main.java
+++ src/es4/Main.java
@@ -1,7 +1,34 @@
 package es4;
 
-public class Main {
-    static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+    static String countdown(int secondi) {
+        String risultato = "";
+
+        for (int i = secondi; i > 0; i--) {
+
+            if (i % 2 == 0) {
+                risultato += "[OK]" + " " + i + "\n";
+            } else {
+                risultato += "[CHECK]" + " " + i + "\n";
+            }
+
+            if (i == 10) {
+                risultato += "--- SEPARAZIONE STADIO ---\n";
+            }
+        }
+        risultato += "--- IGNITION ---\n";
+
+        return risultato;
+    }
+
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci secondi:");
+        int secondi = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(countdown(secondi));
     }
 }
