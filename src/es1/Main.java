f14cd5f7f9811b4e9c751c1e193e9cf2b8357d10
cicalecristian <cicalecristian@gmail.com>
2026-03-10 13:43:22 +0100

    first ex.

--- src/es1/Main.java
+++ src/es1/Main.java
@@ -1,7 +1,27 @@
 package es1;
 
+import java.util.Scanner;
+
 public class Main {
+
+//    static boolean stringaPariDispari(String stringa) {
+//        return stringa.length() % 2 == 0;
+//    }
+
+    static boolean annoBisestile(int anno) {
+        if (anno % 4 != 0) {
+            return false;
+        } else return anno % 100 != 0 || anno % 400 == 0;
+    }
+
     static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Inserisci una stringa: ");
+//        String stringa = scanner.nextLine();
+//        System.out.println(stringaPariDispari(stringa));
+
+        System.out.println("inserisci un anno: ");
+        int anno = Integer.parseInt(scanner.nextLine());
+        System.out.println(annoBisestile(anno));
     }
 }
